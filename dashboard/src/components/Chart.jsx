456441206e9b2d05edf084ee63cb870ba174afd9
luiexe <122733197+luiexe@users.noreply.github.com>
2026-01-14 14:41:30 -0300

    Update Chart.jsx

--- dashboard/src/components/Chart.jsx
+++ dashboard/src/components/Chart.jsx
@@ -1,13 +1,13 @@
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
 export default function Chart({ data }) {
   return (
-    <ResponsiveContainer width="100%" height={300}>
+    <ResponsiveContainer width="100%" height={250}>
       <BarChart data={data}>
         <XAxis dataKey="categoria_tamanho_job" />
         <YAxis />
         <Tooltip />
-        <Bar dataKey="count" fill="#8884d8" />
+        <Bar dataKey="count" fill="#4F46E5" />
       </BarChart>
     </ResponsiveContainer>
   );

4721eb2ec5e8c9171018c007dcec23d8b0762bb8
luiexe <122733197+luiexe@users.noreply.github.com>
2026-01-14 16:59:54 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cors from "cors"; // Recomendado: npm install cors
 import pkg from "pg";
 
 const { Pool } = pkg;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cors()); // Habilita acesso do Dashboard
 

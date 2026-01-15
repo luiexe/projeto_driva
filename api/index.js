24b408f47befca7a49a887b3aabbe6aa3be46f71
luiexe <122733197+luiexe@users.noreply.github.com>
2026-01-14 22:38:21 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -196,6 +196,6 @@ app.get("/analytics/workspaces/top", auth, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("API rodando na porta 3000 🚀");
 });

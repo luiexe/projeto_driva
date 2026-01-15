960eaaf3753945139cfb2f687738dded65d1864f
luiexe <122733197+luiexe@users.noreply.github.com>
2026-01-15 11:17:19 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -153,7 +153,12 @@ app.get("/analytics/enrichments", auth, async (req, res) => {
     const result = await pool.query(dataQuery, [...params, limit, offset]);
 
     res.json({
-      meta: { total_items: Number(totalRes.rows[0].count), page, limit },
+      meta: { 
+        total_items, 
+        total_pages, // Adicionado
+        page, 
+        limit 
+      },
       data: result.rows
     });
   } catch (error) {

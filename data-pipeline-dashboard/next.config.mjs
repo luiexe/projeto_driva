01003a7cb4b4aff59208d1309dc8522840fe457b
luiexe <122733197+luiexe@users.noreply.github.com>
2026-01-15 10:43:00 -0300

    Update next.config.mjs

--- data-pipeline-dashboard/next.config.mjs
+++ data-pipeline-dashboard/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    allowedDevOrigins: ["147.93.67.102", "localhost:5173", "0.0.0.0:3000"]
+  },
 }
 
 export default nextConfig
